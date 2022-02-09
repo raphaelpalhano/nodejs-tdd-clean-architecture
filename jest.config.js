90313416077c4f3440f746b4cd80cdfdb72d7b15
Raphael Angel Palhano Carballar Arevalos <raphael.arevalos@compasso.com.br>
2022-02-09 10:55:33 -0300

    configuration jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jest-environment-node'
+
+}
